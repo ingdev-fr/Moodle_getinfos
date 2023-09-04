cba458cf7168e210b6cd5c8ece3137a4afe3073c
ingdev-fr <damien.will@ingdev.fr>
2023-09-04 22:23:09 +0200

    first commit

---
+++ block_getinfos.php
@@ -0,0 +1,112 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Block getinfos is defined here.
+ *
+ * @package     block_getinfos
+ * @copyright   2023 INGDEV <damien.will@ingdev.fr>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_getinfos extends block_base {
+
+    /**
+     * Initializes class member variables.
+     */
+    public function init() {
+        // Needed by Moodle to differentiate between blocks.
+        $this->title = get_string(identifier:'pluginname', component:'block_getinfos');
+    }
+
+    /**
+     * Returns the block contents.
+     *
+     * @return stdClass The block contents.
+     */
+
+
+      // --------- Ici une fonction pour déclarer si le plugin a un fichier de configuration (settings.php). Ici oui ---------
+    function has_config() {
+        return true;
+    }
+
+
+   // -------------  La fonction qui retourne le contenu du plugin de bloc --------------------------------
+   function get_content() {
+    global $DB; // Moodle l'a initialisée auparavant. Elle permet le dialogue entre le fichier et la BDD
+
+    if ($this->content !== null) {
+        return $this->content;
+    }
+
+    // On initialise une varibale "content" 
+    $content = '';
+
+    // On récupère la valeur du paramètre "showcourses" dans le fichier de settings.php (configuration)
+    $showcourses = get_config(plugin: 'block_getinfos', name: 'showcourses');
+    // Si la valeur est "true", (donc la case est cochée), on affiche les cours dans le bloc
+    if($showcourses) {
+        $courses = $DB->get_records(table:'course'); // Je fetche les données de la table "course"
+        foreach($courses as $course) {
+            $content .= '<option value="'.$course->id.'">'.$course->shortname.' '.$course->fullname.'</option>';
+        }
+    }
+    // Sinon on affiche les users dans le bloc
+    else {
+        $users = $DB->get_records(table:'user'); // Je fetche les données de la table "user"
+        foreach($users as $user) {
+            $content .= '<option value="'.$user->id.'">'.$user->firstname.' '.$user->lastname.'</option>';
+        }
+    }
+
+    if (empty($this->instance)) {
+        $this->content = 'Pas de données recueillies';
+        return $this->content;
+    }
+
+    $this->content = new stdClass();
+    $this->content->text = $content;
+    $this->content->footer = 'This is the footer';
+
+    return $this->content; // On retourne notre objet "content"
+}
+
+    /**
+     * Defines configuration data.
+     *
+     * The function is called immediately after init().
+     */
+    public function specialization() {
+
+        // Load user defined title and make sure it's never empty.
+        if (empty($this->config->title)) {
+            $this->title = get_string('pluginname', 'block_getinfos');
+        } else {
+            $this->title = $this->config->title;
+        }
+    }
+
+    /**
+     * Sets the applicable formats for the block.
+     *
+     * @return string[] Array of pages and permissions.
+     */
+    public function applicable_formats() {
+        return array(
+            'all' => true,
+        );
+    }
+}
---
+++ db/access.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * HTML block caps.
+ *
+ * @package    block_getinfos
+ * @copyright  Damien Will <@ingdev.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// ------------- Définition de qui a le droit de créer ou d'ajouter ce bloc -------------
+$capabilities = array(
+    // Sur le tableau de bord utilisateur
+    'block/getinfos:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW // L'utilisateur a le droit sur son tableau de bord
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    // Sur le reste du site
+    'block/getinfos:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW, //le formateur éditeur et le manager ont le droit d'instancier le bloc sur le site
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
